32b4833b10d0ba9b742622f34b9bd071fbbca473
OctavioNovelo <octavionovelo22@gmail.com>
2025-11-26 16:54:15 -0600

    Banco.json e integracion de bibliotecas

--- Fundamentos de programacion/Ordinario/tienda_regalos.cpp
+++ Fundamentos de programacion/Ordinario/tienda_regalos.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <fstream>
+// #include <jsoncpp/json/json.h>
 
 using namespace std;
 
 int main ()
 {
-    cout << "Hola uwu";
+    ifstream ifs("empleados.json");
 }
 
 //ahuevo listo ya porfavoorrrrrr help
