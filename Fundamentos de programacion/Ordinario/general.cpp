33d539c82d878a6e55b6536310c84bc617b902bc
OctavioNovelo <octavionovelo22@gmail.com>
2025-12-04 03:07:22 -0600

    Implementacion confirmacion mayor a 0 stock en carrito

--- Fundamentos de programacion/Ordinario/general.cpp
+++ Fundamentos de programacion/Ordinario/general.cpp
@@ -302,12 +302,12 @@ int validarMayorACero(){
     int valor;
     while(true) {
         cin>>valor;
-        if(cin.fail()){  //por si escribe letras 
+        /* if(cin.fail()){  //por si escribe letras 
             cin.clear();  
             cin.ignore(numeric_limits<streamsize>::max(), '\n');  //limpia
             cout << "Error; debe ingresar un numero\n";
-        } 
-        else if (valor<=0){
+        } */
+        if (valor<=0){
             cout<<"Error, debe ser mayor a 0\n";
         } 
         else{
