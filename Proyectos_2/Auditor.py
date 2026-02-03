a991e11359a8048386949f46aba59deb63dd17f1
OctavioNovelo <octavionovelo22@gmail.com>
2026-02-03 00:44:29 -0600

    Corrections

--- Proyectos_2/Auditor.py
+++ Proyectos_2/Auditor.py
@@ -1,4 +1,16 @@
-# This is the beginning of the Auditor.py file
+# This is the beginning of the Auditor project
 from frontend.CLI import CLI
+import sys
+from core.compiler import ToolCompiler
+from pathlib import Path
 
-CLI().run()
+def main():
+    if "--compile" in sys.argv:
+        print("[*] Compiling tools...")
+        ToolCompiler(Path(__file__).parent).compile_nmap()
+        return
+
+    CLI().run()
+    
+if __name__ == "__main__":
+    main()
