a983fd1ccc368554211bbfb4c0eb4969ba435873
OctavioNovelo <octavionovelo22@gmail.com>
2026-02-03 12:18:21 -0600

    Corrections

---
+++ Algoritmos/unique_value.py
@@ -0,0 +1,2 @@
+def count_unique_values():
+    
--- Proyectos_2/tools/nmap.py
+++ Proyectos_2/tools/nmap.py
@@ -1,17 +1,17 @@
 class NmapCommands:
 
 # Comandos de nmap.
-    COMMANDS = {
+    commands = {
         1: lambda target: ["-F", target],
         2: lambda target: ["-sV", target],
         3: lambda target: ["-O", target],
         4: lambda target: ["-A", "-p-", target],
     }
 
-# Se verifica si la opcion se encuentra en COMMANDS
+# Se verifica si la opcion se encuentra en commands
     @classmethod
     def build(cls, option: int, target: str):
-        if option not in cls.COMMANDS:
+        if option not in cls.commands:
             raise ValueError("Opción inválida")
 
-        return cls.COMMANDS[option](target)
+        return cls.commands[option](target)
