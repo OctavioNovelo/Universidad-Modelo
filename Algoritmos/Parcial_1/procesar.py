1d66f0c36e12a463f21635c9952973982c783bbc
OctavioNovelo <octavionovelo22@gmail.com>
2026-02-05 21:29:33 -0600

    Tarea_1

--- Algoritmos/Parcial_1/procesar.py
+++ Algoritmos/Parcial_1/procesar.py
@@ -1,24 +1,39 @@
-lista2 = [0 , 0, 0, 0]
-cont = 0
-cont_neg = 0
-prom_neg = 0
+lista2 = [0, 0, 0, []]
 
-def procesar(lista ):
+def procesar(lista):
+    count = 0
+    sum = 0
+    count_neg = 0
+    sum_neg = 0
+    primos = []
+    zero = 0
+    
     for i in range(len(lista)):
         if lista[i] > 0:
-            cont += 1
-            prom += i
-            lista2[0] = prom / cont
-        elif lista[i] < 0:
-            cont_neg += 1
-            prom_neg += i
-            lista2[1] = prom_neg / cont_neg
-        elif lista[i] == 0:
-            lista2[2] += 1
-        if lista[i] == 1 or lista[i] == 2:
-            lista2[3] = [i]
-        elif lista[i] % 2 == 1:
-            lista2[i] = [i]
+            count += 1
+            sum += lista[i]
+            lista2[0] = sum / count
+        if lista[i] < 0:
+            count_neg += 1
+            sum_neg += lista[i]
+            lista2[1] = sum_neg / count_neg
+        if lista[i] == 0:
+            zero += 1
+            
+        lista2[2] = zero
+        
+        n = lista[i]
+        count_primos = 1
+        if n > 2:
+            for j in range(2, n - 1):
+                if n % j == 0:
+                    count_primos += 1
+            if count_primos <= 2:
+                primos.append(lista[i])
+                
+        lista2[3] = primos
+        
     return lista2
 
 print(procesar([-5, 3, 0, 7, 14, -6, 3, 0, -2, 3, 8]))
+print(procesar([2, 0, 0, 1, -4, -3, 0, 5, 11, -7, 9]))
