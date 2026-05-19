ac83cf5d108f2bb7ca2bbb91713095f0a7c95d2a
LitzyAlex <litzyrichaud@gmail.com>
2026-05-18 23:33:42 -0600

    Se agregaron json de disponibilidad

--- Algoritmos/Parcial_2/Proyecto/cli.py
+++ Algoritmos/Parcial_2/Proyecto/cli.py
@@ -17,7 +17,7 @@ def save_json(filename, data):
     
     # Si es la disponibilidad, la guardamos con un formato especial para que siga pareciendo una matriz
     # y no un tripal de mil lineas que no se entiende. Si es cualquier otra cosa, json.dump normal y ya.
-    if filename == 'disponibilidad_profesores.json':
+    if filename == 'disponibilidad_profesores_3.json':
         def format_dispo(dispo):
             rows = []
             for row in dispo:
@@ -93,7 +93,7 @@ materias_semestre_8 = list(asignaturas_semestre_8.keys())
 
 #--------------------------------------------------------------------------
 # Cada día tiene 3 bloques disponibles (True)
-disponibilidad_profesores = load_json('disponibilidad_profesores.json')
+disponibilidad_profesores = load_json('disponibilidad_profesores_3.json')
 #--------------------------------------------------------------------------------
 
 def menu():
@@ -169,7 +169,7 @@ def gestionar_profesores():
                         'max_horas': horas,
                         'disponibilidad': dispo
                     }
-                    save_json('disponibilidad_profesores.json', disponibilidad_profesores)
+                    save_json('disponibilidad_profesores_3.json', disponibilidad_profesores)
                     print("Profesor agregado.")
                 except ValueError:
                     print("Horas inválidas.")
@@ -180,7 +180,7 @@ def gestionar_profesores():
             if nombre:
                 # Borramos con del al profesor
                 del disponibilidad_profesores[nombre]
-                save_json('disponibilidad_profesores.json', disponibilidad_profesores)
+                save_json('disponibilidad_profesores_3.json', disponibilidad_profesores)
                 print(f"Profesor {nombre} eliminado.")
             input("\nPresiona una tecla para continuar...")
         elif op == '4':
@@ -221,7 +221,7 @@ def gestionar_profesores():
                         seleccion_slots = input(": ").split()
                         if not seleccion_slots: continue
                         if seleccion_slots[0] == '0':
-                            save_json('disponibilidad_profesores.json', disponibilidad_profesores)
+                            save_json('disponibilidad_profesores_3.json', disponibilidad_profesores)
                             break
 
                         for s_str in seleccion_slots:
@@ -250,7 +250,7 @@ def gestionar_profesores():
                     print(f"Máximo actual de horas para {nombre}: {disponibilidad_profesores[nombre]['max_horas']}")
                     nuevas_horas = int(input("Ingrese el nuevo máximo de horas: "))
                     disponibilidad_profesores[nombre]['max_horas'] = nuevas_horas
-                    save_json('disponibilidad_profesores.json', disponibilidad_profesores)
+                    save_json('disponibilidad_profesores_3.json', disponibilidad_profesores)
                     print("Máximo de horas actualizado.")
                 except ValueError:
                     print("Entrada inválida.")
