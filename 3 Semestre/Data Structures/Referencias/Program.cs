2b3cbf718aa54959d3e4ae35e08e07d50f728443
OctavioNovelo <octavionovelo22@gmail.com>
2026-08-23 12:56:42 -0600

    Shi

---
+++ 3 Semestre/Data Structures/Referencias/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+// Clasiicacion de tipos de variable
+// Variables
+// -- Valor: Todos los tipos de datos basicos y los struct
+//              "Donde esta la variable esta su contenido"
+// -- Referencia: Clases (class)
+//              "
+
+// En c# las variables valor NO usan new, las referencias si.
+
+// Paso de parametros
+// -- Valor: Se pasa una copia de la variable, el valor original NO cambia.
+// -- Referencia: 
+
+namespace ValueOrReference
+{
+    internal class Program
+    {
+        static void swap(ref int m, ref int n)
+        {
+            int aux = 0;
+            m = n;
+            n = aux;
+        }
+
+        static void Main(string[] args)
+        {
+            int m = 15;
+            int n = 10;
+
+            Console.WriteLine($"m = {m}, n = {n}");
+            Program.swap(ref m, ref n);
+            Console.WriteLine($"m = {m}, n = {n}");
+
+        }
+    }
+}
---
+++ 3 Semestre/Data Structures/Stack_dev/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Una clase static permite acceder a la informacion de una clase SIN tener que
+// crear un objeto de esa clase (<class> <nombre> = new ......)
+
+namespace StackDev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            //int[] data = new int[0];
+
+            //Console.WriteLine("Hola papus!");
+        }
+    }
+}
