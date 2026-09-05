f12635ce4ad2b0a27850ab7b8cc82ea2a54d5f53
OctavioNovelo <octavionovelo22@gmail.com>
2026-09-05 15:05:44 -0600

    SI

--- 3 Semestre/POO/Tarea_1/obj/Debug/net10.0/Tarea_1.AssemblyInfo.cs
+++ 3 Semestre/POO/Tarea_1/obj/Debug/net10.0/Tarea_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da80a8c398ceaeda878f89532280121e55c61bea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854029cba3ba6761827f13ebfb743efdc7de8190")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
